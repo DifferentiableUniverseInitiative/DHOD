fb3745317da2f644035267f2783d7bbab283ab70
Francois <francois@orioncloud.fr>
2020-04-03 23:38:23 +0200

    Adds a fix

--- setup.py
+++ setup.py
@@ -8,6 +8,5 @@ setup(name = 'diffhod',
       python_requires='>3.5.2',
       description = 'differentiable HOD',
       packages=find_packages(),
-      install_requires = ['tensorflow', 'tensorflow-probability==0.9'],
-      tests_require = ['halotools']
-        )
+      install_requires = ['tensorflow', 'tensorflow-probability==0.9', 'flowpm'],
+      tests_require = ['halotools'])
