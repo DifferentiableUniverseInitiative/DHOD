0f7793c453f15a35607a7719c774c05b0fc5d50a
Ben Horowitz <horowitz.ben@gmail.com>
2020-06-19 04:40:23 +0900

    basic batch functions, basic optimization example still works

--- diffhod/components.py
+++ diffhod/components.py
@@ -4,7 +4,6 @@ import tensorflow_probability as tfp
 tfd = tfp.distributions
 tfb = tfp.bijectors
 from diffhod.distributions import NFW
-
 def Zheng07Cens(halo_mvir,
                 logMmin=ed.Deterministic(11.35, name='logMmin'),
                 sigma_logM=ed.Deterministic(0.25, name='sigma_logM'),
@@ -12,7 +11,7 @@ def Zheng07Cens(halo_mvir,
                 name='zheng07Cens', **kwargs):
   halo_mvir = tf.math.log(halo_mvir) / tf.math.log(10.)
   # Compute the mean number of centrals
-  p = tf.clip_by_value(0.5 * (1+tf.math.erf((halo_mvir - logMmin)/sigma_logM)), 1.e-4, 1-1.e-4)
+  p = tf.clip_by_value(0.5 * (1+tf.math.erf((halo_mvir - tf.reshape(logMmin,(-1,1)))/tf.reshape(sigma_logM,(-1,1)))), 1.e-4, 1-1.e-4)
   return ed.RelaxedBernoulli(temperature, probs=p, name=name)
 
 def Zheng07SatsPoisson(halo_mvir,
@@ -21,26 +20,30 @@ def Zheng07SatsPoisson(halo_mvir,
                 logM1=ed.Deterministic(12.4, name='logM1'),
                 alpha=ed.Deterministic(0.83, name='alpha'),
                 name='zheng07Sats', **kwargs):
-  M0 = 10.**logM0
-  M1 = 10.**logM1
-  rate = n_cen.distribution.probs * ((halo_mvir - M0)/M1)**alpha
-  rate = tf.where(halo_mvir < M0, 1e-4, rate)
+  M0 = tf.pow(10.,logM0)
+  M1 = tf.pow(10.,logM1)
+  rate = n_cen.distribution.probs * ((halo_mvir - tf.reshape(M0,(-1,1)))/tf.reshape(M1,(-1,1)))**tf.reshape(alpha,(-1,1))
+  rate = tf.where(halo_mvir < tf.reshape(M0,(-1,1)), 1e-4, rate)
   return ed.Poisson(rate=rate, name=name)
 
 def Zheng07SatsRelaxedBernoulli(halo_mvir,
-                n_cen,
-                sample_shape,
-                logM0=ed.Deterministic(11.2, name='logM0'),
-                logM1=ed.Deterministic(12.4, name='logM1'),
-                alpha=ed.Deterministic(0.83, name='alpha'),
-                temperature=0.2,
-                name='zheng07Sats', **kwargs):
-  M0 = 10.**logM0
-  M1 = 10.**logM1
-  rate = n_cen.distribution.probs * (tf.nn.relu(halo_mvir - M0)/M1)**alpha
-  return ed.RelaxedBernoulli(temperature=temperature,
+        n_cen,
+        sample_shape,
+        logM0=ed.Deterministic(11.2, name='logM0'),
+        logM1=ed.Deterministic(12.4, name='logM1'),
+        alpha=ed.Deterministic(0.83, name='alpha'),
+        temperature=0.2,
+        name='zheng07Sats', **kwargs):
+    M0 = tf.pow(10.,logM0)
+    M1 = tf.pow(10.,logM1)
+    print(M0)
+    rate = n_cen.distribution.probs * (tf.nn.relu(halo_mvir - tf.reshape(M0,(-1,1)))/tf.reshape(M1,(-1,1)))**tf.reshape(alpha,(-1,1))
+
+    return ed.RelaxedBernoulli(temperature=temperature,
                              probs=tf.clip_by_value(rate/sample_shape[0],1.e-5,1-1e-4),
                              sample_shape=sample_shape)
+
+
 
 def NFWProfile(pos,
                concentration,
