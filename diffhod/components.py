bd22a0f32a0fbbdadadb6d1c9255620bb04f6d12
EiffL <fr.eiffel@gmail.com>
2021-08-04 19:39:37 +0200

    Adds documentation to components and cleanup

--- diffhod/components.py
+++ diffhod/components.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 import edward2 as ed
 import tensorflow_probability as tfp
@@ -28,13 +29,23 @@ def Zheng07Cens(Mhalo,
   Returns:
     ncentral. RandomVariable: Number of central galaxy in the halo.
   """
-  Mhalo = tf.math.log(Mhalo) / tf.math.log(10.)  # log10(Mhalo)
+  with tf.name_scope('Zheng07Cens'):
+    Mhalo = tf.convert_to_tensor(Mhalo)
+    logMmin = tf.convert_to_tensor(logMmin)
+    sigma_logM = tf.convert_to_tensor(sigma_logM)
+    # Test for batched parameters
+    shape_params = logMmin.get_shape()
+    if len(shape_params) > 0:
+      logMmin = tf.reshape(logMmin, [-1, 1])
+      sigma_logM = tf.reshape(sigma_logM, [-1, 1])
 
-  # Compute the mean number of centrals
-  p = tf.clip_by_value(0.5 * (1 + tf.math.erf((Mhalo - logMmin) / sigma_logM)),
-                       1.e-4, 1 - 1.e-4)
+    Mhalo = tf.math.log(Mhalo) / np.log(10.)  # log10(Mhalo)
 
-  return ed.RelaxedBernoulli(temperature, probs=p, name=name)
+    # Compute the mean number of centrals
+    p = tf.clip_by_value(0.5 * (1 + tf.math.erf((Mhalo - logMmin) / sigma_logM)),
+                        1.e-4, 1 - 1.e-4)
+
+    return ed.RelaxedBernoulli(temperature, probs=p, name=name)
 
 
 def _Zheng07SatsRate(Mhalo, logM0, logM1, alpha):
@@ -54,10 +65,22 @@ def Zheng07SatsPoisson(Mhalo,
 
     <Nsat> = <Ncen> ((Mh - M0)/M1)^alpha 
   '''
-  M0 = tf.pow(10., logM0)
-  rate = Ncen.distribution.probs * _Zheng07SatsRate(Mhalo, logM0, logM1, alpha)
-  rate = tf.where(Mhalo < M0, 1e-4, rate)
-  return ed.Poisson(rate=rate, name=name)
+  with tf.name_scope('Zheng07Sats'):
+    Mhalo = tf.convert_to_tensor(Mhalo)
+    logM0 = tf.convert_to_tensor(logM0)
+    logM1 = tf.convert_to_tensor(logM1)
+    alpha = tf.convert_to_tensor(alpha)
+    # Test for batched parameters
+    shape_params = logM0.get_shape()
+    if len(shape_params) > 0:
+      logM0 = tf.expand_dims(logM0, -1)
+      logM1 = tf.expand_dims(logM1, -1)
+      alpha = tf.expand_dims(alpha, -1)
+
+    M0 = tf.pow(10., logM0)
+    rate = Ncen.distribution.probs * _Zheng07SatsRate(Mhalo, logM0, logM1, alpha)
+    rate = tf.where(Mhalo < M0, 1e-4, rate)
+    return ed.Poisson(rate=rate, name=name)
 
 
 def Zheng07SatsRelaxedBernoulli(Mhalo,
@@ -84,24 +107,38 @@ def Zheng07SatsRelaxedBernoulli(Mhalo,
   Returns:
     nsat. array_like RandomVariable: Tensor of shape `sample_shape` with binary entries
   """
-  rate = Ncen.distribution.probs * _Zheng07SatsRate(Mhalo, logM0, logM1, alpha)
-  return ed.RelaxedBernoulli(temperature=temperature,
-                             probs=tf.clip_by_value(rate / sample_shape[0],
-                                                    1.e-5, 1 - 1e-4),
-                             sample_shape=sample_shape,
-                             name=name)
+  with tf.name_scope('Zheng07Sats'):
+    Mhalo = tf.convert_to_tensor(Mhalo)
+    logM0 = tf.convert_to_tensor(logM0)
+    logM1 = tf.convert_to_tensor(logM1)
+    alpha = tf.convert_to_tensor(alpha)
+    # Test for batched parameters
+    shape_params = logM0.get_shape()
+    if len(shape_params) > 0:
+      logM0 = tf.expand_dims(logM0, -1)
+      logM1 = tf.expand_dims(logM1, -1)
+      alpha = tf.expand_dims(alpha, -1)
 
+    rate = Ncen.distribution.probs * _Zheng07SatsRate(Mhalo, logM0, logM1, alpha)
+    return ed.RelaxedBernoulli(temperature=temperature,
+                              probs=tf.clip_by_value(rate / sample_shape[0],
+                                                      1.e-5, 1 - 1e-4),
+                              sample_shape=sample_shape,
+                              name=name)
 
-def NFWProfile(center,
+# By default, we will refer to the Bernoulli model as Zheng07Sats                        
+Zheng07Sats = Zheng07SatsRelaxedBernoulli
+
+def NFWProfile(pos,
                concentration,
                Rvir,
-               sample_shape,
+               sample_shape=(100, ),
                name='positions',
                **kwargs):
   """ Cartesian coordinates drawn from isotropic NFW profile.
 
   Args:
-    center: Tensor of shape [..., 3]; halo center.
+    pos: Tensor of shape [..., 3]; halo center.
     concentration: float; concentration parameter for NFW profile
     Rvir: float; Virial radius of halo
     sample_shape: maximum number of satellite per halo.
@@ -109,16 +146,19 @@ def NFWProfile(center,
   Returns:
     positions. array_like RandomVariable of cartesian coordinates.
   """
-  pos = ed.RandomVariable(tfd.TransformedDistribution(
-      distribution=tfd.VonMisesFisher(
-          tf.one_hot(tf.zeros_like(concentration, dtype=tf.int32), 3), 0),
-      bijector=tfb.Shift(center)(tfb.Scale(
-          tf.expand_dims(ed.RandomVariable(NFW(concentration,
-                                               Rvir,
-                                               name='radius'),
-                                           sample_shape=sample_shape),
-                         axis=-1))),
-      name='position'),
-                          sample_shape=sample_shape,
-                          name=name)
-  return pos
+  with tf.name_scope('NFWProfile'):
+    pos = tf.convert_to_tensor(pos)
+    concentration = tf.convert_to_tensor(concentration)
+    Rvir = tf.convert_to_tensor(Rvir)
+
+    return ed.RandomVariable(tfd.TransformedDistribution(
+        distribution=tfd.VonMisesFisher(
+            tf.one_hot(tf.zeros_like(concentration, dtype=tf.int32), 3), 0),
+        bijector=tfb.Shift(pos)(tfb.Scale(
+            tf.expand_dims(ed.RandomVariable(NFW(concentration,
+                                                Rvir,
+                                                name='radius'),
+                                            sample_shape=sample_shape),
+                          axis=-1))),
+        name=name),
+                            sample_shape=sample_shape)
